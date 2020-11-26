6a1a8d4d77c69563e8dd3731f70545122cfe6afc
onyxcode <me@onyxcode.net>
2020-11-26 14:47:43 -0500

    Epic poggers, v1a

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # hazy
-A simple weather CLI/GUI program made in Python.
+A simple weather GUI program made in Python.
+
+# Prerequisites
+- Any version of Python 3.x, up to Python 3.8.6
+- An OpenWeatherMap API key. You can get one for free [here](https://home.openweathermap.org/users/sign_up).
+
+# Installation
+
+**Attention!** No matter what method you use, make sure you have a creds.json file in the same folder as the app, otherwise it will not run.
+
+Sample config.json file:
+
+```json
+{
+  "owm_key":"<your key here>"
+}
+```
+
+## Preferred Method - Pre-built binaries
+You can download the prebuilt binaries from the [Releases page](https://github.com/onyxcode/hazy/releases).
+
+## Alternative Method - Run from Source
+If you like rooting through source code like me, you can download the script in it's raw form. Whether this is to look for security flaws, add enhancements or just copy the code, I made the source available for download because after all, this is GitHub. *Psst, you can copy my code by the way, just be sure to credit me :D*
+
+Clone the repo to your computer by typing:
+```sh
+git clone https://github.com/onyxcode/hazy.git
+# Or, if you have github-cli installed:
+gh repo clone onyxcode/hazy
+```
+Then, cd into the directory and run `python3 app.py`.
---
+++ app.py
@@ -0,0 +1,65 @@
+import json
+import PySimpleGUI as sg
+import pyowm
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
+sg.theme('BluePurple')
+
+with open("creds.json") as creds:
+    creds = json.load(creds)
+
+owm = OWM(creds["owm_key"])
+mgr = owm.weather_manager()
+
+
+layout = [  [sg.Text('Check the weather in your area!')], [sg.Text('What is your location?'), sg.InputText()], [sg.Button('Go'), sg.Button('Exit')] ]
+
+window = sg.Window('Hazy - GUI', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Go': # if user closes window or clicks cancel
+        break
+    if event == 'Exit':
+        exit(0)
+
+try:
+    observation = mgr.weather_at_place(values[0])
+except pyowm.commons.exceptions.NotFoundError:
+    layout = [ [sg.Text(f'An error has occured!')], [sg.Text(f'The location you have entered is not recognized. The program will now exit.')], [sg.Button('OK')] ]
+
+    window = sg.Window('Hazy - Error', layout)
+
+    while True:
+        event, values = window.read()
+        if event == 'OK':
+            exit(0)
+else:
+    w = observation.weather
+    will_it_rain = mgr.forecast_at_place(values[0], '3h')
+    today = timestamps.tomorrow()
+    rain_q  = will_it_rain.will_be_rainy_at(today)
+
+    if rain_q:
+        but_will_it_rain_tomorrow = "It is likely to rain tomorrow."
+    else:
+        but_will_it_rain_tomorrow = "It doesn't look like it will rain tomorrow."
+
+    window.close()
+
+    windspeed = (w.wind())
+
+    temp = round(w.temperature("fahrenheit")["temp"])
+
+    realfeel = round(w.temperature("fahrenheit")["feels_like"])
+
+    layout =  [  [sg.Text(f'Current weather for {values[0]}')], [sg.Text(f'{w.status} with winds at {windspeed["speed"]}mph.')], [sg.Text(f'Temperature: {temp}° with a RealFeel of {realfeel}°')], [sg.Text(f'Humidity: {w.humidity}%')], [sg.Button('Exit')] ]
+
+    window = sg.Window('Hazy - GUI', layout)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
+            break
